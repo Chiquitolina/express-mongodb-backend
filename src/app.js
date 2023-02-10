d962a7bc7c454776ca53b2e8184f40677a2bf599
Alejandro <aleig.906@gmail.com>
2023-02-10 11:27:48 -0300

    expresshb & views & routess

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 import express from 'express'
-
-import routes from '../routes/index';
+import routes from './routes/index';
+import {engine} from 'express-handlebars'
+import path from 'path'
 
 const app = express()
 
+app.engine('handlebars', engine({
+    extname: '.handlebars'
+    }));
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'handlebars')
 app.use(routes)
 
 export default app
